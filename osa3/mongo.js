9761bfb75488acb3ed79a409fff094fb5893b0f9
Victoria Stadnitski <victoria@goodahead.com>
2026-01-23 20:54:04 +0200

    osa3 3.13-3.14: fetch and save persons from MongoDB

---
+++ osa3/mongo.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = encodeURIComponent(process.argv[2])
+
+
+const url =
+  `mongodb+srv://phonebook:${password}@cluster0.jqkaztf.mongodb.net/phonebook?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length === 5) {
+  const name = process.argv[3]
+  const number = process.argv[4]
+
+  const person = new Person({
+    name,
+    number,
+  })
+
+  person.save().then(() => {
+    console.log(`added ${name} number ${number} to phonebook`)
+    mongoose.connection.close()
+  })
+} else {
+  console.log('phonebook:')
+
+  Person.find({}).then(result => {
+    result.forEach(person => {
+      console.log(`${person.name} ${person.number}`)
+    })
+    mongoose.connection.close()
+  })
+}
